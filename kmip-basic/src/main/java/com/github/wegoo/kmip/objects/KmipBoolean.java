83fe06473eb43e894927e76a11ad235750cb29d4
zhenwei1108 <zhenwei1108@163.com>
2023-07-16 21:45:16 +0800

    布尔

---
+++ kmip-basic/src/main/java/com/github/wegoo/kmip/objects/KmipBoolean.java
@@ -0,0 +1,29 @@
+package com.github.wegoo.kmip.objects;
+
+import com.github.wegoo.kmip.enums.TypeEnum;
+
+/**
+ * @author: zhangzhenwei
+ * @description: com.github.wegoo.kmip.objects 布尔
+ * @date: 2023/7/16  13:50
+ * @since: 1.0.0
+ */
+public class KmipBoolean extends AbstractKmipObject<Boolean>{
+
+  boolean value;
+
+  @Override
+  byte[] getBytesValue() {
+    return new byte[]{(byte) (value ? 0x01 : 0x00)};
+  }
+
+  @Override
+  public TypeEnum getType() {
+    return TypeEnum.KmipBoolean;
+  }
+
+  @Override
+  public Boolean getValue() {
+    return value;
+  }
+}
