a0505a1da968259a5e1509e54a3040060fe2fc27
zhenwei1108 <zhenwei1108@163.com>
2023-07-16 13:49:36 +0800

    add object int\long

---
+++ kmip-basic/src/main/java/com/github/wegoo/kmip/objects/KmipInteger.java
@@ -0,0 +1,33 @@
+package com.github.wegoo.kmip.objects;
+
+import com.github.wegoo.bytes.tools.BytesTools;
+import com.github.wegoo.kmip.enums.TagEnum;
+import com.github.wegoo.kmip.enums.TypeEnum;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author: wegoo
+ * @description: com.github.wegoo.kmip.objects
+ * @date: 2023/7/9  12:03
+ * @since: 1.0.0
+ */
+@AllArgsConstructor
+public class KmipInteger extends AbstractKmipObject<Integer> {
+
+  int value;
+
+  @Override
+  public TypeEnum getType() {
+    return TypeEnum.KmipInteger;
+  }
+
+  @Override
+  public Integer getValue() {
+    return this.value;
+  }
+
+  @Override
+  byte[] getBytesValue() {
+    return BytesTools.intToBytes(getValue());
+  }
+}
---
+++ kmip-basic/src/main/java/com/github/wegoo/kmip/objects/KmipLong.java
@@ -0,0 +1,32 @@
+package com.github.wegoo.kmip.objects;
+
+import com.github.wegoo.bytes.tools.BytesTools;
+import com.github.wegoo.kmip.enums.TagEnum;
+import com.github.wegoo.kmip.enums.TypeEnum;
+
+/**
+ * @author: zhangzhenwei
+ * @description: com.github.wegoo.kmip.objects 长整形
+ * @date: 2023/7/16  13:24
+ * @since: 1.0.0
+ */
+public class KmipLong extends AbstractKmipObject<Long>{
+
+  long value;
+
+  @Override
+  byte[] getBytesValue() {
+    return BytesTools.longToBytes(value);
+  }
+
+
+  @Override
+  public TypeEnum getType() {
+    return TypeEnum.KmipLong;
+  }
+
+  @Override
+  public Long getValue() {
+    return value;
+  }
+}
